be705be0414ac6fe1935a33b4edcdb10ec959a50
Nixie Skywalker <nyxscream@gmail.com>
2018-08-14 21:26:03 +0200

    modify navbar colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild,  ElementRef, ViewEncapsulation } from '@angular/core';
 // import * as MyScriptJS from 'myscript';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +9,36 @@ import { Component, OnInit, ViewChild,  ElementRef, ViewEncapsulation } from '@a
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+    title = 'app';
+    anon: boolean;
+    user: any;
+    loading = true;
 
 //   @ViewChild("tref", {read: ElementRef}) domEditor: ElementRef;
 //   editor;
 
-  constructor() {}
+    constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    // // your code
-    // console.log(this.domEditor.nativeElement);
-    // this.editor = MyScriptJS.register(this.domEditor.nativeElement, {
-    //     recognitionParams: {
-    //         type: 'TEXT',
-    //         protocol: 'WEBSOCKET',
-    //         apiVersion: 'V4',
-    //         server: {
-    //             scheme: 'https',
-    //             host: 'webdemoapi.myscript.com',
-    //             applicationKey: '659eb1cd-d1ee-4834-9cf3-aeaf3fa921dc',
-    //             hmacKey: '0aaae1e2-b681-4508-a8c7-95a3a82507e2',
-    //         },
-    //     },
-    // });
-  }
+    ngOnInit() {
+        this.authService.userChange$.subscribe(user => {
+            this.loading = false;
+            this.user = user;
+            this.anon = !user;
+        });
+        // // your code
+        // console.log(this.domEditor.nativeElement);
+        // this.editor = MyScriptJS.register(this.domEditor.nativeElement, {
+        //     recognitionParams: {
+        //         type: 'TEXT',
+        //         protocol: 'WEBSOCKET',
+        //         apiVersion: 'V4',
+        //         server: {
+        //             scheme: 'https',
+        //             host: 'webdemoapi.myscript.com',
+        //             applicationKey: '659eb1cd-d1ee-4834-9cf3-aeaf3fa921dc',
+        //             hmacKey: '0aaae1e2-b681-4508-a8c7-95a3a82507e2',
+        //         },
+        //     },
+        // });
+    }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,23 +8,45 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-    anon: boolean;
-    user: any;
-    loading = true;
 
-    constructor(private authService: AuthService, private router: Router) { }
+    user: any;
+    anon: boolean;
+
+    transparentClass: boolean = true;
+    darkClass: boolean = true;
+
+    setClasses() {
+        let classes =  {
+            homeClass: this.transparentClass,  
+            secondClass: this.darkClass, 
+        };
+        return classes;
+    }
+
+    constructor(private authService: AuthService, private router: Router) {}
 
     ngOnInit() {
-        this.authService.userChange$.subscribe((user) => {
-            this.loading = false;
+        this.authService.userChange$.subscribe(user => {
             this.user = user;
             this.anon = !user;
-          });
+        });
+
+        this.router.events.subscribe(e => {
+            if(e instanceof NavigationEnd) {
+                if(this.router.url === '/' || this.router.url === '/home') {
+                    this.transparentClass = true;
+                    this.darkClass = false;
+                } else {
+                    this.transparentClass = false;
+                    this.darkClass = true;
+                }
+            }
+        });
     }
 
     logout() {
         this.authService.logout()
-            .then(() => this.router.navigate(['/login']));
+            .then(() => this.router.navigate(['/']));
     }
 
 }

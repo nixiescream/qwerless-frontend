b3c3c4cd54f7704db45d1d246979aefb79b1fce2
Nixie Skywalker <nyxscream@gmail.com>
2018-08-14 14:26:13 +0200

    hide myscript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, ViewChild,  ElementRef, ViewEncapsulation } from '@angular/core';
-import * as MyScriptJS from 'myscript';
+// import * as MyScriptJS from 'myscript';
 
 import { AuthService } from './services/auth.service';
 
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
   loading = true;
   anon: boolean;
   user: any;
-  @ViewChild("tref", {read: ElementRef}) domEditor: ElementRef;
-  editor;
+//   @ViewChild("tref", {read: ElementRef}) domEditor: ElementRef;
+//   editor;
 
   constructor(
     private authService: AuthService,
@@ -29,21 +29,21 @@ export class AppComponent implements OnInit {
       this.anon = !user;
     });
 
-    // your code
-    console.log(this.domEditor.nativeElement);
-    this.editor = MyScriptJS.register(this.domEditor.nativeElement, {
-     recognitionParams: {
-       type: 'TEXT',
-       protocol: 'WEBSOCKET',
-       apiVersion: 'V4',
-       server: {
-         scheme: 'https',
-         host: 'webdemoapi.myscript.com',
-         applicationKey: '659eb1cd-d1ee-4834-9cf3-aeaf3fa921dc',
-         hmacKey: '0aaae1e2-b681-4508-a8c7-95a3a82507e2',
-       },
-     },
-   });
+    // // your code
+    // console.log(this.domEditor.nativeElement);
+    // this.editor = MyScriptJS.register(this.domEditor.nativeElement, {
+    //     recognitionParams: {
+    //         type: 'TEXT',
+    //         protocol: 'WEBSOCKET',
+    //         apiVersion: 'V4',
+    //         server: {
+    //             scheme: 'https',
+    //             host: 'webdemoapi.myscript.com',
+    //             applicationKey: '659eb1cd-d1ee-4834-9cf3-aeaf3fa921dc',
+    //             hmacKey: '0aaae1e2-b681-4508-a8c7-95a3a82507e2',
+    //         },
+    //     },
+    // });
   }
 
   logout() {

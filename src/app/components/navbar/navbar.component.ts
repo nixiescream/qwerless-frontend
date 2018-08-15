507d1db15b75a9c2652b148e3c5273084387536f
Nixie Skywalker <nyxscream@gmail.com>
2018-08-15 16:43:17 +0200

    modify navbar style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,14 +3,22 @@ import { AuthService } from '../../services/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css'],
+    host: {
+        '(window:scroll)': 'onScroll($event)'
+    }
 })
 export class NavbarComponent implements OnInit {
 
     user: any;
     anon: boolean;
+
+    isScrolled = false;
+    currPos: Number = 0;
+    startPos: Number = 0;
+    changePos: Number = 100;
 
     transparentClass: boolean = true;
     darkClass: boolean = true;
@@ -24,6 +32,15 @@ export class NavbarComponent implements OnInit {
     }
 
     constructor(private authService: AuthService, private router: Router) {}
+
+    onScroll(evt) {
+        this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+        if(this.currPos >= this.changePos ) {
+            this.isScrolled = true;
+        } else {
+            this.isScrolled = false;
+        }
+    }
 
     ngOnInit() {
         this.authService.userChange$.subscribe(user => {

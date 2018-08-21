c11a34d738de7dfa397ba6177fa3129765b655b6
Nixie Skywalker <nyxscream@gmail.com>
2018-08-21 12:18:53 +0200

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
     production: false,
     API_URL: 'http://localhost:3000/api',
+    // API_URL: 'http://qwerless-server.herokuapp.com/api',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/qwerless/image/upload',
     CLOUDINARY_UPLOAD_PRESET: 'n8cl7xab',
     CLOUDINADY_IMAGE: 'http://res.cloudinary.com/qwerless/image/upload/v1534788909/'

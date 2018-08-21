2f60abba43a34b9d5e21e2e64df056a142671299
Nixie Skywalker <nyxscream@gmail.com>
2018-08-21 13:45:04 +0200

    fixing bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'https://qwerless-server.herokuapp.com',
+    API_URL: 'https://qwerless-server.herokuapp.com/api',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/qwerless/image/upload',
     CLOUDINARY_UPLOAD_PRESET: 'n8cl7xab',
     CLOUDINADY_IMAGE: 'http://res.cloudinary.com/qwerless/image/upload/v1534788909/'

9d935028f5b0cb9ffb2f04c5440c1fc7ea863ce9
Nixie Skywalker <nyxscream@gmail.com>
2018-08-22 14:41:15 +0200

    random changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     API_URL: 'https://qwerless-server.herokuapp.com/api',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/qwerless/image/upload',
     CLOUDINARY_UPLOAD_PRESET: 'n8cl7xab',
-    CLOUDINADY_IMAGE: 'http://res.cloudinary.com/qwerless/image/upload/v1534788909/'
+    CLOUDINADY_IMAGE: 'https://res.cloudinary.com/qwerless/image/upload/v1534788909/'
 };

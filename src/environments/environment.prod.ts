4e93583645923eafdd348767fb076e27d3fd677c
Nixie Skywalker <nyxscream@gmail.com>
2018-08-23 12:25:27 +0200

    sdkjhkjdsh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/qwerless/image/upload',
     CLOUDINARY_UPLOAD_PRESET: 'n8cl7xab',
     CLOUDINADY_IMAGE: 'https://res.cloudinary.com/qwerless/image/upload/v1534788909/',
-    DOMAIN_NAME: 'https://qwerless-server.herokuapp.com:8080'
+    DOMAIN_NAME: 'https://qwerless-server.herokuapp.com:80'
 };
